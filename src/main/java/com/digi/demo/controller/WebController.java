9be06d2d6be392bc90f8745ca9792363981c85e5
brotherlw <levin.wilke05@gmail.com>
2025-09-24 14:29:53 +0200

    implementation of the navbar

--- src/main/java/com/digi/demo/controller/WebController.java
+++ src/main/java/com/digi/demo/controller/WebController.java
@@ -1,14 +1,22 @@
 package com.digi.demo.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class WebController {
 
     @GetMapping("/")
-    public String home() {
-        return "index";
+    public RedirectView home() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && auth.isAuthenticated() && !"anonymousUser".equals(auth.getPrincipal())) {
+            return new RedirectView("/dashboard");
+        } else {
+            return new RedirectView("/register");
+        }
     }
 
     @GetMapping("/login")
@@ -24,5 +32,25 @@ public class WebController {
     @GetMapping("/dashboard")
     public String dashboard() {
         return "pages/dashboard";
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        return "pages/profile";
+    }
+
+    @GetMapping("/store")
+    public String store() {
+        return "pages/store";
+    }
+
+    @GetMapping("/posts")
+    public String posts() {
+        return "pages/posts";
+    }
+
+    @GetMapping("/friends")
+    public String friends() {
+        return "pages/friends";
     }
 }
