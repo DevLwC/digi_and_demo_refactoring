2da8fb46e121cc5df00bd31b6e26a2569264a318
brotherlw <levin.wilke05@gmail.com>
2025-10-01 15:24:07 +0200

    adding of the logo for the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,35 @@
 import React from 'react'
-        import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-        import Profile from './pages/Profile.jsx'
-        import Register from './pages/Register.jsx'
-        import Store from './pages/Store.jsx'
-        import Dashboard from './pages/Dashboard.jsx'
-        import Friends from './pages/Friends.jsx'
-        import Posts from './pages/Posts.jsx'
-        import Login from './pages/Login.jsx'
-        import Navbar from './components/Navbar.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Profile from './pages/Profile.jsx'
+import Register from './pages/Register.jsx'
+import Store from './pages/Store.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import Friends from './pages/Friends.jsx'
+import Posts from './pages/Posts.jsx'
+import Login from './pages/Login.jsx'
+import Navbar from './components/Navbar.jsx'
+import Logo from './components/Logo.jsx'
 
-        function App() {
-          return (
-            <Router>
-              <div style={{ paddingBottom: '60px' }}>
+function App() {
+    return (
+        <Router>
+            <Logo />
+            <div style={{ paddingBottom: '60px' }}>
                 <Routes>
-                  <Route path="/" element={<Dashboard />} />
-                  <Route path="/dashboard" element={<Dashboard />} />
-                  <Route path="/profile" element={<Profile />} />
-                  <Route path="/register" element={<Register />} />
-                  <Route path="/store" element={<Store />} />
-                  <Route path="/friends" element={<Friends />} />
-                  <Route path="/posts" element={<Posts />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="*" element={<div>404 Not Found</div>} />
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/store" element={<Store />} />
+                    <Route path="/friends" element={<Friends />} />
+                    <Route path="/posts" element={<Posts />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="*" element={<div>404 Not Found</div>} />
                 </Routes>
                 <Navbar />
-              </div>
-            </Router>
-          )
-        }
+            </div>
+        </Router>
+    )
+}
 
-        export default App
+export default App
