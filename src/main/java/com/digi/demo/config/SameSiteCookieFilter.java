757e6e14323669c37fbfe4cd4b03857928868853
brotherlw <levin.wilke05@gmail.com>
2025-10-13 11:45:42 +0200

    so schön neu

--- src/main/java/com/digi/demo/config/SameSiteCookieFilter.java
+++ src/main/java/com/digi/demo/config/SameSiteCookieFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import org.springframework.stereotype.Component;
 import java.io.IOException;
+import java.util.Collection;
 
 @Component
 public class SameSiteCookieFilter implements Filter {
@@ -13,9 +14,18 @@ public class SameSiteCookieFilter implements Filter {
         chain.doFilter(request, response);
         if (response instanceof HttpServletResponse) {
             HttpServletResponse res = (HttpServletResponse) response;
-            for (String header : res.getHeaders("Set-Cookie")) {
-                if (header.startsWith("JSESSIONID")) {
-                    res.setHeader("Set-Cookie", header + "; SameSite=None; Secure");
+            Collection<String> headers = res.getHeaders("Set-Cookie");
+            boolean firstHeader = true;
+            for (String header : headers) {
+                String updatedHeader = header;
+                if (!header.toLowerCase().contains("samesite")) {
+                    updatedHeader = header + "; SameSite=None; Secure";
+                }
+                if (firstHeader) {
+                    res.setHeader("Set-Cookie", updatedHeader);
+                    firstHeader = false;
+                } else {
+                    res.addHeader("Set-Cookie", updatedHeader);
                 }
             }
         }
