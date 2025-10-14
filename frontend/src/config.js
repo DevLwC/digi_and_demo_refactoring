663aa8994ac7f4f3a43598e4b12c258c7b7c7486
brotherlw <levin.wilke05@gmail.com>
2025-10-14 16:23:33 +0200

    pwa fun

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-//export const API_BASE_URL = 'https://digi-and-demo-refactoring-aci.westeurope.azurecontainer.io';
+export const API_BASE_URL = 'https://digi-and-demo-refactoring-aci.westeurope.azurecontainer.io';
 
-export const API_BASE_URL = 'https://localhost:443';
+//export const API_BASE_URL = 'https://localhost:443';
